1af8f7653da5589bf10e2f51bde28ae2502d98fd
Maukrack <majiqtresi@gmail.com>
2021-10-31 01:43:30 -0500

    triangle.pl is working correctly

--- src_homework/triangle.pl
+++ src_homework/triangle.pl
@@ -3,19 +3,24 @@
 %% values should be positive
 %% To be a triangle X + Y should be greater than Z, otherwise is not a triangle. 
 
-triangle(X, Y, Z) :-
+valid_triangle(X, Y, Z) :-
     X > 0, Y > 0, Z > 0,
-    X + Y > Z, 
-    X =:= Y, Y =:= Z,
+    X + Y > Z, X + Z > Y, Y + Z > X.
+
+triangle(X, Y, Z) :-
+    valid_triangle(X, Y, Z),
+    X =:= Y, X =:= Z, Y =:= Z,
     write('triangule is equilateral').
 
 triangle(X, Y, Z) :-
-    X > 0, Y > 0, Z > 0,
-    X + Y > Z, 
+    valid_triangle(X, Y, Z),
     \+ (X = Y), \+ (X = Z), \+ (Y = Z),
     write('triangule is scalene').
 
 triangle(X, Y, Z) :-
-    X > 0, Y > 0, Z > 0,
-    X + Y > Z, 
+    valid_triangle(X, Y, Z),
     write('triangule is isosceles').
+
+triangle(X, Y, Z) :-
+    \+ (valid_triangle(X, Y, Z)),
+    write('this values does not correspond to a triangle').
